19f3bdde925eb22eadfca66cbfe16e2fa7c12cb1
Md. Atiqur Rahman <atiqur.su@gmail.com>
2022-04-01 12:02:42 +0600

    #updated - localize scripts

--- src/Handlers/ScriptHandler.php
+++ src/Handlers/ScriptHandler.php
@@ -45,15 +45,42 @@ class ScriptHandler implements EnqueueInterface {
 	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
 	 * @var bool
 	 */
-	private bool $isInFooter = false;
-	private bool $isInline = false;
-	private array $inlineVal = [];
+	private bool $isInFooter = true;
 
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var string
+	 */
 	private string $flName;
+
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var string|null
+	 */
 	private ?string $changePath = '';
+
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var string
+	 */
 	private string $version;
+
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var string
+	 */
 	private string $objName;
+
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var bool
+	 */
 	private bool $hasLocal = false;
+
+	/**
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @var array
+	 */
 	private array $local;
 
 
@@ -74,6 +101,20 @@ class ScriptHandler implements EnqueueInterface {
 		return $this->handle;
 	}
 
+	/**
+	 * setHandle is responsible for doing some .
+	 *
+	 * @param string $handle
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @rel - YXRpcXVyLnN1QGdtYWlsLmNvbQ!#
+	 *
+	 * @return $this
+	 * @cache - TWQuIEF0aXF1ciBSYWhtYW4!
+	 * @author - pavenest solutions
+	 * @email - pavenest@gmail.com
+	 * @created - 1/4/22 - 11:58 AM
+	 *
+	 */
 	public function setHandle(string $handle): ScriptHandler {
 		$this->handle = $handle;
 
@@ -103,18 +144,19 @@ class ScriptHandler implements EnqueueInterface {
 	/**
 	 * inFooter is responsible for doing some .
 	 *
+	 * @param $newVal
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @rel - YXRpcXVyLnN1QGdtYWlsLmNvbQ!#
+	 *
 	 * @return $this
 	 * @cache - TWQuIEF0aXF1ciBSYWhtYW4!
 	 * @author - pavenest solutions
 	 * @email - pavenest@gmail.com
-	 * @created - 30/3/22 - 4:40 PM
-	 *
-	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
-	 * @rel - YXRpcXVyLnN1QGdtYWlsLmNvbQ!#
+	 * @created - 1/4/22 - 11:59 AM
 	 *
 	 */
-	public function inFooter(): ScriptHandler {
-		$this->isInFooter = true;
+	public function inFooter($newVal = true): ScriptHandler {
+		$this->isInFooter = $newVal;
 
 		return $this;
 	}
@@ -161,6 +203,21 @@ class ScriptHandler implements EnqueueInterface {
 		return $this;
 	}
 
+	/**
+	 * localize is responsible for doing some .
+	 *
+	 * @param string $objName
+	 * @param array $val
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @rel - YXRpcXVyLnN1QGdtYWlsLmNvbQ!#
+	 *
+	 * @return $this
+	 * @cache - TWQuIEF0aXF1ciBSYWhtYW4!
+	 * @author - pavenest solutions
+	 * @email - pavenest@gmail.com
+	 * @created - 1/4/22 - 12:00 PM
+	 *
+	 */
 	public function localize(string $objName, array $val): ScriptHandler {
 
 		$this->objName  = $objName;
@@ -170,6 +227,19 @@ class ScriptHandler implements EnqueueInterface {
 		return $this;
 	}
 
+	/**
+	 * call is responsible for doing some .
+	 *
+	 * @return bool
+	 * @cache - TWQuIEF0aXF1ciBSYWhtYW4!
+	 * @author - pavenest solutions
+	 * @email - pavenest@gmail.com
+	 * @created - 1/4/22 - 12:00 PM
+	 *
+	 * @praam - cGF2ZW5lc3RAZ21haWwuY29t
+	 * @rel - YXRpcXVyLnN1QGdtYWlsLmNvbQ!#
+	 *
+	 */
 	public function call(): bool {
 		$app = Application::getInstance();
 
@@ -181,12 +251,11 @@ class ScriptHandler implements EnqueueInterface {
 			$loc = $app->getBaseUrl() . $this->changePath . $this->flName;
 		}
 
-
 		wp_register_script($this->handle, $loc, $this->deps, $this->version, $this->isInFooter);
 
-		if($this->hasLocal) {
-			wp_localize_script($this->handle, $this->objName, $this->local);
-		}
+//		if($this->hasLocal) {
+//			wp_localize_script($this->handle, $this->objName, $this->local);
+//		}
 
 		return true;
 	}
@@ -217,7 +286,7 @@ class ScriptHandler implements EnqueueInterface {
 			wp_enqueue_script($this->handle);
 
 			if($this->hasLocal) {
-				//wp_localize_script($this->handle, $this->objName, $this->local);
+				wp_localize_script($this->handle, $this->objName, $this->local);
 			}
 		}
 	}
